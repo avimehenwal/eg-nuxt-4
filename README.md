a37310556b108d5c43bc79a177bde2e9bd653f67
avimehenwal <avi.mehanwal@gmail.com>
2020-03-30 03:50:30 +0200

    Read and write cookies

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # eg-nuxt-4
 
+* works with both public and private sheets
+* Ranges could extend
+
+```bash
+curl -H "Accept: application/json" -H "Authorization: Bearer ya29.a0Ae4lvC1HgrRIG1qkXC135g8GJyJXPagwrtgwQfoJqfq2ZfiIMR9I1M7-2GFrz38f034SuKK5u159RV6JfsnT0DnozMpDphlO-P0Kloh6Hwfttc6ZBANORiJJTrSHSowE8p9lBdziouy9BOu9g_8OJYwDd4fuxKU5rbY" https://sheets.googleapis.com/v4/spreadsheets/1e-s4_8qx5rdKg2ABfd_WUfnYuOrRN0TvgAlsBLTiaeY/values/Sheet1!A1:D3
+```
+
 > My awe-inspiring Nuxt.js project
 
 ## Build Setup
---
+++ pages/sheets.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <h1>Sheets</h1>
+    <v-btn @click="sheet">get sheet </v-btn>
+    {{ this.$auth.token }}
+    {{ token }}
+  </v-container>
+</template>
+
+<script>
+// const { google } = require('googleapis')
+// let url = 'https://sheets.googleapis.com/v4/spreadsheets/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/values/Sheet1!A1:D5'
+const Cookie = process.client ? require('js-cookie') : undefined
+
+export default {
+  data () {
+    return {
+      token: null
+    }
+  },
+  methods: {
+    sheet () {
+      Cookie.set('foo', 'bar')
+      this.token = Cookie.get('auth._token.google')
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
