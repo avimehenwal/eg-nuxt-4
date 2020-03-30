57601f22637b88f401267c12d846b8a776ffe0d4
avimehenwal <avi.mehanwal@gmail.com>
2020-03-30 05:12:24 +0200

    Add drive scope and sample drive call with querystring

--- README.md
+++ README.md
@@ -4,7 +4,17 @@
 * Ranges could extend
 
 ```bash
+# Google sheets
 curl -H "Accept: application/json" -H "Authorization: Bearer ya29.a0Ae4lvC1HgrRIG1qkXC135g8GJyJXPagwrtgwQfoJqfq2ZfiIMR9I1M7-2GFrz38f034SuKK5u159RV6JfsnT0DnozMpDphlO-P0Kloh6Hwfttc6ZBANORiJJTrSHSowE8p9lBdziouy9BOu9g_8OJYwDd4fuxKU5rbY" https://sheets.googleapis.com/v4/spreadsheets/1e-s4_8qx5rdKg2ABfd_WUfnYuOrRN0TvgAlsBLTiaeY/values/Sheet1!A1:D3
+
+# Google drive
+curl \
+  'https://www.googleapis.com/drive/v3/about?fields=*&key=ya29.a0Adw1xeVM1gEzhaYHH-a6Fb829YqLhW6RRd6JTBAVJZ7z6o21rxcVYpcLbSvlV0YzZbXJPsPw0k1RiCaJa4dg1bFLHieiCPBXFnytVAc2q7KiY6rTuccfVKooQxYA9RkccFxxor7ioEzxg6l0RkpMgxVv0sw7nvt1Ats' \
+  --header 'Accept: application/json' \
+  --header 'Authorization: Bearer ya29.a0Adw1xeVM1gEzhaYHH-a6Fb829YqLhW6RRd6JTBAVJZ7z6o21rxcVYpcLbSvlV0YzZbXJPsPw0k1RiCaJa4dg1bFLHieiCPBXFnytVAc2q7KiY6rTuccfVKooQxYA9RkccFxxor7ioEzxg6l0RkpMgxVv0sw7nvt1Ats' \
+  --compressed
+
+
 ```
 
 > My awe-inspiring Nuxt.js project
--- pages/sheets.vue
+++ pages/sheets.vue
@@ -3,35 +3,46 @@
     <h1>Sheets</h1>
     <v-btn @click="sheet">get sheet </v-btn>
     <v-card tile>
-      <v-list-item> {{ token }} </v-list-item>
+      <!-- <v-list-item> {{ token }} </v-list-item> -->
       <v-list-item v-for="item in data" :key="item">
         <v-list-item-content>
           <v-list-item-title>{{item}}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-card>
+    {{driveData}}
   </v-container>
 </template>
 
 <script>
 // const { google } = require('googleapis')
 // let url = 'https://sheets.googleapis.com/v4/spreadsheets/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/values/Sheet1!A1:D5'
+// const querystring = require('querystring')
 const Cookie = process.client ? require('js-cookie') : undefined
-const url = 'https://sheets.googleapis.com/v4/spreadsheets/1KuuDpw2ssi4EC-hlkOTWQTFuIhVGtlN8EhqsdI4y2S0/values/INDEX!A1:F3'
+// const url = 'https://sheets.googleapis.com/v4/spreadsheets/1KuuDpw2ssi4EC-hlkOTWQTFuIhVGtlN8EhqsdI4y2S0/values/INDEX!A1:F3'
 export default {
   data () {
     return {
       token: null,
-      data: null
+      data: null,
+      driveData: null
     }
   },
   methods: {
     sheet () {
       Cookie.set('foo', 'bar')
       this.token = Cookie.get('auth._token.google')
+      // this.$axios.setToken(this.token)
+      // this.$axios.get(url)
+      //   .then(response => (this.data = response.data.values))
+      // const drive = 'https://www.googleapis.com/drive/v3/about?fields=*'
+      const driveParams = {
+        fields: '*'
+      }
+      const drive = 'https://www.googleapis.com/drive/v3/about'
       this.$axios.setToken(this.token)
-      this.$axios.get(url)
-        .then(response => (this.data = response.data.values))
+      this.$axios.get(drive, { params: driveParams })
+        .then(response => (this.driveData = response.data))
     }
   }
 }
